3db80300ee7ba7177b4ee1934c5ac29eb5af609d
Ana <analucia.spi02@gmail.com>
2020-05-20 13:59:27 -0400

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ReactComponent as ReactLogo} from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactLogo />
+    </div>
+  );
+}
+
+export default App;
