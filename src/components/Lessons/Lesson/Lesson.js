65db5dd1ef24d95f64fbfe8e40ebfc92fe54b140
Ana <analucia.spi02@gmail.com>
2020-06-06 16:28:15 -0400

    fix card and images

--- src/components/Lessons/Lesson/Lesson.js
+++ src/components/Lessons/Lesson/Lesson.js
@@ -7,8 +7,13 @@ export default class Lesson extends Component {
     render() {
         const {id, title, img} = this.props.lesson;
         return (
-            <div className="lesson-card">
-                <h3>Lesson</h3>
+            <div className="lesson-card" onClick={() => console.log('you clicked on the card')}>
+                <Link to="/details">
+                    <div className="lesson-container">
+                        <img src={img} alt="lesson" className="lesson-img"/>
+                    </div>
+                    <h4 className="lesson-title">{title}</h4>
+                </Link>
             </div>
         )
     }
